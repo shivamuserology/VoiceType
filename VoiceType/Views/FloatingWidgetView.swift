eb39f3ad90e0bf667a618769db10af6bffb5a11d
shivamuserology <shivamsethi1610@gmail.com>
2026-01-17 01:09:40 +0530

    UI: Change stop button background to red

--- VoiceType/Views/FloatingWidgetView.swift
+++ VoiceType/Views/FloatingWidgetView.swift
@@ -107,7 +107,7 @@ struct RecordingPillView: View {
             Button(action: onStop) {
                 ZStack {
                     Circle()
-                        .fill(Color.white.opacity(0.2))
+                        .fill(Color.red)
                         .frame(width: 16, height: 16) // Scale 28->16
                     
                     RoundedRectangle(cornerRadius: 2)
