c75e919e8223b01174f8a049aad7ab47cee62c74
shivamuserology <shivamsethi1610@gmail.com>
2026-01-17 01:02:14 +0530

    UI: Adjust floating widget Y offset to 5px from bottom

--- VoiceType/Views/FloatingPanel.swift
+++ VoiceType/Views/FloatingPanel.swift
@@ -82,7 +82,7 @@ class FloatingPanel: NSPanel {
         let windowWidth: CGFloat = 300
         
         let x = screenFrame.midX - (windowWidth / 2)
-        let y = screenFrame.minY + 20 // 20px from bottom (was 50)
+        let y = screenFrame.minY + 5 // 5px from bottom (was 20)
         
         self.setFrame(NSRect(x: x, y: y, width: windowWidth, height: 30), display: true)
     }
